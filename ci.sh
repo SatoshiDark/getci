6f2f1219e67501a75f16a3ef5962cb6f828dee0f
Satoshi Bruno <ygon01@gmail.com>
2019-10-28 15:36:21 -0400

    First Commit

---
+++ ci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Print helpFunction in case parameters are empty
+if [ -z "$START" ] || [ -z "$END" ]
+then
+   echo "Usage: Define START and END as env variables, also the URL";
+   echo "Downloads for ci 1 to 100";
+   exit
+fi
+echo "Output file: ci-$START-$END.csv"
+touch ci-$START-$END.csv
+BASE_URL=$URL
+
+i=1
+while [ $i -lt $END ]
+do
+  echo $i
+  URL="${BASE_URL}${i}"
+  RES=$(curl -s "$URL")
+  if grep -q numMesa <<<"$RES"; then
+      echo "$i    $RES" >> ./results/ci-$START-$END.csv
+  fi
+  i=`expr $i + 1`
+done
+echo "Complete"
